36c6b3f393093d9c7ca738aede65e7e16b7f0709
HyperActiveHub <coolkimch@gmail.com>
2020-01-15 01:32:46 +0100

    Crossfade from Camera to RenderTexture working.
    Note that UI RawImage - color can bug out and only show all white, this is fixed by fiddling with the alpha back and forth (supposed to be all white with full alpha).

--- Assets/Scripts/SceneTransistion.cs
+++ Assets/Scripts/SceneTransistion.cs
@@ -5,17 +5,76 @@ using UnityEngine;
 public class SceneTransistion : MonoBehaviour
 {
     public RenderTexture rt;
+    public GameObject go;
+    public float fadeTime;
 
+    float timer;
+    bool fadeOut, fadeIn;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F12))
+        if (Input.GetKeyDown(KeyCode.F12))
         {
-            ScreenCapture.CaptureScreenshotIntoRenderTexture(rt);
+            //ScreenCapture.CaptureScreenshotIntoRenderTexture(rt);
+            var tex = ScreenCapture.CaptureScreenshotAsTexture();
+            go.GetComponent<UnityEngine.UI.RawImage>().material.mainTexture = tex;
+        }
+        else if (Input.GetKeyDown(KeyCode.F11))
+        {
+            fadeOut = true;
+            fadeIn = false;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.F10))
+        {
+            fadeIn = true;
+            fadeOut = false;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (fadeOut)
+        {
+            float a = 1;
+            if (timer < fadeTime)
+            {
+                timer += Time.deltaTime;
+                var t = timer / (fadeTime + 0.001f);
+                a = Mathf.Lerp(1, 0, t);
+            }
+            else
+            {
+                timer = 0;
+                a = 0;
+                fadeOut = false;
+            }
+
+            var c = go.GetComponent<UnityEngine.UI.RawImage>().material.color;
+            go.GetComponent<UnityEngine.UI.RawImage>().material.color = new Color(c.r, c.g, c.b, a);
+        }
+        else if (fadeIn)
+        {
+            float a = 0;
+            if (timer < fadeTime)
+            {
+                timer += Time.deltaTime;
+                var t = timer / (fadeTime + 0.001f);
+                a = Mathf.Lerp(0, 1, t);
+            }
+            else
+            {
+                timer = 0;
+                a = 1;
+                fadeIn = false;
+            }
+
+            var c = go.GetComponent<UnityEngine.UI.RawImage>().material.color;
+            go.GetComponent<UnityEngine.UI.RawImage>().material.color = new Color(c.r, c.g, c.b, a);
         }
     }
 }
